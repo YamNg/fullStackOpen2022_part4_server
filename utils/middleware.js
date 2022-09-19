3f6cd2a263eead1b406f462fe649030c4eca23f2
Yam Ng <yamng1994@gmail.com>
2022-09-19 15:32:52 -0400

    bug fix on handling error in middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -15,35 +15,56 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  logger.info('hit yam')
   logger.error(error.message)
 
-  if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message 
+    })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
 
   next(error)
 }
 
 const tokenExtractor = (request, response, next) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    request.token = authorization.substring(7)
-  } else {
-    request.token = null
+  try{
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      request.token = authorization.substring(7)
+    } else {
+      request.token = null
+    }
+    next()
+  } catch (error){
+    next(error)
   }
-  next()
 }
 
 const userExtractor = async (request, response, next) => {
-  const token = request.token
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  try{
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    request.user = user
+    next()
+  } catch (error){
+    next(error)
   }
-  const user = await User.findById(decodedToken.id)
-  request.user = user
-  next()
 }
 
 module.exports = {
